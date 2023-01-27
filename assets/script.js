517cb689df2448b51f20589c8b254b25882424a6
SimonTinhofer <sim.tinhofer@billrothgymnasium.at>
2023-01-27 19:08:44 +0100

    dasd

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,3 @@
-/*
-class Table {
-    constructor(length){
-        this.students = new Array(length);
-    }
-    get length(){
-        return this.students.length;
-    }
-    getStudent(index){
-        return this.students[index];
-    }
-}
-class Room {
-    // [[2,2,2] , [2,2,2], [2,2,2,2]]
-    constructor(structure){
-    }
-    getTable()
-}
-*/
-
-
-
   
 let nameList = [
     "Amro",
@@ -81,12 +59,9 @@ var nrOfStudents = nameList.length;
 console.log(nrOfPlaces)
 while(nrOfStudents < nrOfPlaces)
 {
-    console.log("Juhu")
     nameList.push("leerer Platz")
     var nrOfStudents = nameList.length;
 }
-console.log(nameList)
-
 
 // Array mischen
 
@@ -101,18 +76,16 @@ function shuffle(array) {
       currentIndex--;
   
       // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+      [array[currentIndex], array[randomIndex]] = 
+      [array[randomIndex], array[currentIndex]];
     }
   
     return array
 }
-
+var a = "saod";
 
 function changeColor(newColor) {
     shuffle(nameList)
-    console.log(nameList)
-    
     // Schülernamen aus array ziehen und plätze einzeln durchlaufen
     let s = 0;
     for(let i = 0; i < TableList.length; i++){
@@ -121,31 +94,17 @@ function changeColor(newColor) {
             s += 2;
         }
     }
-    console.log(TableList)
-    const elem0 = document.getElementById('t00');
-    elem0.textContent = TableList[0][0]
-    const elem1 = document.getElementById('t01');
-    elem1.textContent = TableList[1][0]
-    const elem2 = document.getElementById('t02');
-    elem2.textContent = TableList[2][0]
-    const elem3 = document.getElementById('t10');
-    elem3.textContent = TableList[0][1]
-    const elem4 = document.getElementById('t11');
-    elem4.textContent = TableList[1][1]
-    const elem5 = document.getElementById('t12');
-    elem5.textContent = TableList[2][1]
-    const elem6 = document.getElementById('t20');
-    elem6.textContent = TableList[0][2]
-    const elem7 = document.getElementById('t21');
-    elem7.textContent = TableList[1][2]
-    const elem8 = document.getElementById('t30');
-    elem8.textContent = TableList[0][3]
-    const elem9 = document.getElementById('t31');
-    elem9.textContent = TableList[1][3]
-    const elem10 = document.getElementById('t40');
-    elem10.textContent = TableList[0][4]
-    
-    
+    document.getElementById('t00').textContent = TableList[0][0];
+    document.getElementById('t01').textContent = TableList[1][0];
+    document.getElementById('t02').textContent = TableList[2][0];
+    document.getElementById('t10').textContent = TableList[0][1];
+    document.getElementById('t11').textContent = TableList[1][1];
+    document.getElementById('t12').textContent = TableList[2][1];
+    document.getElementById('t20').textContent = TableList[0][2];
+    document.getElementById('t21').textContent = TableList[1][2];
+    document.getElementById('t30').textContent = TableList[0][3];
+    document.getElementById('t31').textContent = TableList[1][3];
+    document.getElementById('t40').textContent = TableList[0][4];
   }
 
 
